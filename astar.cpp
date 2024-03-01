0000d12103a7db24ab495d13a7c3be156de1e17d
chris <ivi3001@yahoo.de>
2024-03-01 17:56:10 +0100

    added comments

--- astar.cpp
+++ astar.cpp
@@ -1,39 +1,45 @@
+#include <algorithm>
+#include <cmath>
 #include <iomanip> // for std::setw
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <unordered_map>
-#include <cmath>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-class Point {
+class Point
+{
 public:
     int x, y;
 
-    bool operator==(const Point& o) const {
+    bool operator==(const Point &o) const
+    {
         return x == o.x && y == o.y;
     }
 
-    bool operator!=(const Point& o) const {
+    bool operator!=(const Point &o) const
+    {
         return !(*this == o);
     }
 
-    bool operator<(const Point& o) const {
+    bool operator<(const Point &o) const
+    {
         return x < o.x || (x == o.x && y < o.y);
     }
 };
 
-class AStar {
+class AStar
+{
     struct Node {
         Point coordinates;
-        Node* parent;
-        float gCost; // cost from start to current node
-        float hCost; // heuristic cost estimate to end
+        Node *parent;
+        float gCost;                                  // cost from start to current node
+        float hCost;                                  // heuristic cost estimate to end
         float fCost() const { return gCost + hCost; } // total cost
 
-        Node(Point coord_, Node* parent_ = nullptr) : coordinates(coord_), parent(parent_), gCost(0), hCost(0) {}
+        Node(Point coord_, Node *parent_ = nullptr) :
+            coordinates(coord_), parent(parent_), gCost(0), hCost(0) {}
     };
 
     Point start, end;
@@ -41,44 +47,49 @@ class AStar {
     int gridWidth, gridHeight;
 
 public:
-    AStar(int width, int height, const Point& start_, const Point& end_) : gridWidth(width), gridHeight(height), start(start_), end(end_) {
+    AStar(int width, int height, const Point &start_, const Point &end_) :
+        gridWidth(width), gridHeight(height), start(start_), end(end_)
+    {
         grid.resize(gridWidth, vector<bool>(gridHeight, true));
     }
 
-    void setWall(int x, int y) {
+    void setWall(int x, int y)
+    {
         if (x >= 0 && x < gridWidth && y >= 0 && y < gridHeight) {
             grid[x][y] = false;
         }
     }
 
     struct CompareNode {
-        bool operator()(const Node* lhs, const Node* rhs) const {
+        bool operator()(const Node *lhs, const Node *rhs) const
+        {
             return lhs->fCost() > rhs->fCost();
         }
     };
 
     struct PointHash {
-        size_t operator()(const Point& p) const {
+        size_t operator()(const Point &p) const
+        {
             return hash<long long>()(((long long)p.x << 32) | p.y);
         }
     };
 
-    vector<Point> findPath() {
+    vector<Point> findPath()
+    {
         vector<Point> path;
-        priority_queue<Node*, vector<Node*>, CompareNode> openSet;
-        //priority_queue<Node*, vector<Node*>, [](const Node* lhs, const Node* rhs) { return lhs->fCost() > rhs->fCost(); }> openSet;
-        //unordered_map<Point, Node*, [](const Point& p) { return hash<long long>()(((long long)p.x << 32) | p.y); }> allNodes;
-        unordered_map<Point, Node*, PointHash> allNodes;
+        priority_queue<Node *, vector<Node *>, CompareNode> openSet;
+        // priority_queue<Node*, vector<Node*>, [](const Node* lhs, const Node* rhs) { return lhs->fCost() > rhs->fCost(); }> openSet;
+        // unordered_map<Point, Node*, [](const Point& p) { return hash<long long>()(((long long)p.x << 32) | p.y); }> allNodes;
+        unordered_map<Point, Node *, PointHash> allNodes;
 
-
-        Node* startNode = new Node(start);
+        Node *startNode = new Node(start);
         startNode->gCost = 0;
         startNode->hCost = heuristic(start, end);
         openSet.push(startNode);
         allNodes[start] = startNode;
 
         while (!openSet.empty()) {
-            Node* currentNode = openSet.top();
+            Node *currentNode = openSet.top();
             openSet.pop();
 
             if (currentNode->coordinates == end) {
@@ -89,36 +100,37 @@ public:
                 break;
             }
 
-
-
             for (int x = -1; x <= 1; x++) {
                 for (int y = -1; y <= 1; y++) {
                     if (x == 0 && y == 0) {
                         continue; // Skip the current node itself
                     }
-            
+
                     // Coordinates of neighbout
                     Point newCoordinates(currentNode->coordinates.x + x, currentNode->coordinates.y + y);
-            
+
                     // Check if the new coordinates are within the grid and not a wall
                     if (!isInBounds(newCoordinates) || !grid[newCoordinates.x][newCoordinates.y]) {
                         continue;
                     }
-            
+
                     // Straight: |x+y|  = 1
                     // Diagonal: |x+y| != 1
-                    //int newGCost = currentNode->gCost + ((x != 0 && y != 0) ? diagonalCost : straightCost);
-                    float newGCost = currentNode->gCost + ((abs(x+y) == 1) ? straightCost : diagonalCost);
+                    // int newGCost = currentNode->gCost + ((x != 0 && y != 0) ? diagonalCost : straightCost);
+                    float newGCost = currentNode->gCost + ((abs(x + y) == 1) ? straightCost : diagonalCost);
 
-                    Node* successor = new Node(newCoordinates, currentNode);
+                    Node *successor = new Node(newCoordinates, currentNode); // Node(coord, parent)
                     successor->gCost = newGCost;
                     successor->hCost = heuristic(successor->coordinates, end);
-            
 
-                    if (Node* existingNode = allNodes[successor->coordinates]; existingNode != nullptr) {
+                    // If allNodes does not contain 'successor->coordinates' it will create a new element
+                    // wich is a Node*. And this pointer will be a nullptr.
+                    if (Node *existingNode = allNodes[successor->coordinates]; existingNode != nullptr) {
+                        // existingNode was not created by default constructor
+                        // this Node* was alrady in allNodes
                         if (existingNode->gCost <= successor->gCost) {
                             delete successor;
-                            continue;
+                            continue; // stop executing here and go the next interation
                         }
                         allNodes.erase(successor->coordinates);
                     }
@@ -130,15 +142,16 @@ public:
         }
 
         // Cleanup
-        for (auto& pair : allNodes) {
+        for (auto &pair : allNodes) {
             delete pair.second;
         }
 
         reverse(path.begin(), path.end());
         return path;
     }
-    
-    bool isWalkable(int x, int y) const {
+
+    bool isWalkable(int x, int y) const
+    {
         if (x >= 0 && x < gridWidth && y >= 0 && y < gridHeight) {
             return grid[x][y];
         }
@@ -146,28 +159,30 @@ public:
     }
 
 private:
-    float diagonalCost = sqrt(2); //1.42; 
-    float straightCost = 1; 
-    
-    static float heuristic(const Point& a, const Point& b) {
-        //return fabs(a.x - b.x) + abs(a.y - b.y);
+    float diagonalCost = sqrt(2); // 1.42;
+    float straightCost = 1;
+
+    static float heuristic(const Point &a, const Point &b)
+    {
+        // return fabs(a.x - b.x) + abs(a.y - b.y);
         float dx = a.x - b.x;
         float dy = a.y - b.y;
-        return sqrt(dx*dx + dy*dy) + abs(a.y - b.y);
+        return sqrt(dx * dx + dy * dy) + abs(a.y - b.y);
     }
 
-    bool isInBounds(const Point& p) const {
+    bool isInBounds(const Point &p) const
+    {
         return p.x >= 0 && p.x < gridWidth && p.y >= 0 && p.y < gridHeight;
     }
 };
 
-
-int main() {
+int main()
+{
     int width = 10, height = 10;
     Point start(5, 0), end(9, 9);
     AStar astar(width, height, start, end);
-    
-    //AStar astar(10, 10, {0, 0}, {9, 9});
+
+    // AStar astar(10, 10, {0, 0}, {9, 9});
 
     // Setting walls in the grid
     astar.setWall(1, 1);
@@ -179,16 +194,16 @@ int main() {
     astar.setWall(5, 3);
     astar.setWall(6, 3);
     astar.setWall(6, 2);
-    
+
     astar.setWall(6, 1);
     astar.setWall(6, 0);
     astar.setWall(0, 6);
     astar.setWall(1, 6);
     astar.setWall(2, 6);
-    //astar.setWall(3, 6);
+    // astar.setWall(3, 6);
     astar.setWall(5, 9);
     astar.setWall(5, 8);
-    
+
     astar.setWall(9, 8);
     astar.setWall(8, 8);
     astar.setWall(7, 8);
@@ -209,7 +224,7 @@ int main() {
     }
 
     // Mark the path on the grid
-    for (const auto& p : path) {
+    for (const auto &p : path) {
         if (p != start && p != end) {
             displayGrid[p.x][p.y] = '.';
         }
@@ -226,11 +241,9 @@ int main() {
         }
         cout << endl;
     }
-    
+
     cout << endl;
     cout << "path length: " << path.size() << endl;
 
     return 0;
 }
-
-
